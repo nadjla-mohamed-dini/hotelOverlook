37a78e6ecd132c1b73487b2d957f962b97a94838
nadjla-mohamed-dini <nadjla.mohamed-dini@laplateforme.io>
2025-09-28 14:36:12 +0200

    add event reservation presence and review reposit'

---
+++ repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.models.Event;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    
+}
