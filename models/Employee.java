a60747ccb13261c2264d1efa3b23699ee20ddefc
nadjla-mohamed-dini <nadjla.mohamed-dini@laplateforme.io>
2025-09-28 14:30:26 +0200

    add some models files

---
+++ models/Employee.java
@@ -0,0 +1,39 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(name="last_name")
+    private String lastName;
+    
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private AppUser user;
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    
+    public AppUser getUser(){return user;}
+    public void setUser(AppUser user) {this.user = user;} 
+}
